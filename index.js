b45d2e306bb8b9a1fbfb052b1959f24a3ffca72d
James Talmage <james@talmage.io>
2015-10-31 17:42:34 -0400

    a minimal working nyc

--- index.js
+++ index.js
@@ -2,77 +2,14 @@
 
 module.exports = promisify;
 
-var Promise = require('native-or-bluebird');
-
-var PROXIED_METHODS = ['exists', 'val', 'hasChild', 'hasChildren', 'numChildren', 'getPriority', 'exportVal'];
-
-function promisify(proto, addSync, accessor) {
-	if (typeof addSync === 'function') {
-		accessor = addSync;
-		addSync = false;
-	}
-
-	PROXIED_METHODS.forEach(function (methodName) {
-		addMethod(methodName);
-	});
-
-	addMethod(null, 'snap');
-
-	function addMethod(methodOnSnap, methodOnProto) {
-		methodOnProto = methodOnProto || methodOnSnap;
-		proto[methodOnProto] = makeAsyncMethod(methodOnSnap, accessor);
-		if (addSync) {
-			proto[methodOnProto + 'Sync'] = makeSyncMethod(methodOnSnap, accessor);
-		}
-	}
-}
-
-function makeAsyncMethod(methodOnSnap, accessor) {
-	accessor = accessor || thisAccessor;
-
-	return function () {
-		var args = methodOnSnap && Array.prototype.slice.call(arguments);
-		var ref = accessor.call(this);
+function promisify(proto) {
+	proto.exists = function () {
+		var ref = this;
 		return new Promise(function (resolve, reject) {
 			ref.once('value', function (snap) {
-				try {
-					var result = methodOnSnap ? snap[methodOnSnap].apply(snap, args) : snap;
-					resolve(result);
-				} catch (e) {
-					reject(e);
-				}
+				var result = snap.exists();
+				resolve(result);
 			}, reject);
 		});
 	};
-}
-
-function makeSyncMethod(methodOnSnap, accessor) {
-	accessor = accessor || thisAccessor;
-
-	return function () {
-		var args = methodOnSnap && Array.prototype.slice.call(arguments);
-		var called = false;
-		var value;
-		var ref = accessor.call(this);
-
-		ref.once('value',
-			function (snap) {
-				called = true;
-				value = methodOnSnap ? snap[methodOnSnap].apply(snap, args) : snap;
-			},
-			function (err) {
-				throw err;
-			}
-		);
-
-		if (!called) {
-			throw new Error('value listener was not called synchronously');
-		}
-
-		return value;
-	};
-}
-
-function thisAccessor() {
-	return this;
 }
--- test.js
+++ test.js
@@ -2,69 +2,22 @@ import test from 'ava';
 import promisify from './';
 import Firebase from 'firebase';
 
-Firebase.goOffline();
+test('async methods', t => {
+	t.plan(2);
 
-promisify(Firebase.prototype, true);
+	function blah(exists) {
+		this.once = (type, fn) => {
+			t.ok(type === 'value');
+			setImmediate(() => fn({
+				exists: () => exists
+			}))
+		};
+	}
 
-function baseRef() {
-	return new Firebase('https://test.firebaseio.test');
-}
+	promisify(blah.prototype);
 
-test('async methods', async t => {
-	t.plan(15);
-	var ref = baseRef();
-	ref.set(null);
-	ref.child('a').setWithPriority('b', 3);
+	var obj = new blah(true);
 
-	t.ok(await ref.child('a').exists() === true);
-	t.ok(await ref.child('b').exists() === false);
-
-	t.same(await ref.val(), {a: 'b'});
-	t.ok(await ref.child('a').val() === 'b');
-
-	t.ok(await ref.hasChild('a') === true);
-	t.ok(await ref.hasChild('b') === false);
-
-	t.ok(await ref.hasChildren() === true);
-	t.ok(await ref.child('a').hasChildren() === false);
-
-	t.ok(await ref.numChildren() === 1);
-	t.ok(await ref.child('a').numChildren() === 0);
-
-	t.ok(await ref.getPriority() === null);
-	t.ok(await ref.child('a').getPriority() === 3);
-
-	t.same(await ref.exportVal(), {a: {'.value': 'b', '.priority': 3}});
-	t.same((await ref.snap()).exportVal(), {a: {'.value': 'b', '.priority': 3}});
-	t.same(await ref.child('a').exportVal(), {'.value': 'b', '.priority': 3});
-});
-
-test('sync methods', t => {
-	t.plan(15);
-	var ref = baseRef();
-	ref.set(null);
-	ref.child('a').setWithPriority('b', 3);
-
-	t.ok(ref.child('a').existsSync() === true);
-	t.ok(ref.child('b').existsSync() === false);
-
-	t.same(ref.valSync(), {a: 'b'});
-	t.ok(ref.child('a').valSync() === 'b');
-
-	t.ok(ref.hasChildSync('a') === true);
-	t.ok(ref.hasChildSync('b') === false);
-
-	t.ok(ref.hasChildrenSync() === true);
-	t.ok(ref.child('a').hasChildrenSync() === false);
-
-	t.ok(ref.numChildrenSync() === 1);
-	t.ok(ref.child('a').numChildrenSync() === 0);
-
-	t.ok(ref.getPrioritySync() === null);
-	t.ok(ref.child('a').getPrioritySync() === 3);
-
-	t.same(ref.exportValSync(), {a: {'.value': 'b', '.priority': 3}});
-	t.same(ref.snapSync().exportVal(), {a: {'.value': 'b', '.priority': 3}});
-	t.same(ref.child('a').exportValSync(), {'.value': 'b', '.priority': 3});
+	obj.exists().then(exists => t.ok(exists === true));
 });
 
